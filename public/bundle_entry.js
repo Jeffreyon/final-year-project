2f43bbc0ba50df3114fbaad1d491980da985c8fd
jeffrey <jeffreyon11@gmail.com>
2022-12-09 09:46:43 +0100

    changes

--- public/bundle_entry.js
+++ public/bundle_entry.js
@@ -4,6 +4,9 @@ require("./css/custom.css");
 
 // js files
 window.$ = require("jquery");
+const noUiSlider = require("./vendor/nouislider/nouislider");
+const owlCarousel = require("./vendor/owlcarousel/owl.carousel.js");
+
 require("./js/global");
 require("./js/layouts_auth");
 require("./js/layouts_dashboard");
@@ -11,6 +14,3 @@ require("./js/layouts_main");
 require("./js/nouislider_config");
 require("./js/owlcarousel_config");
 require("./js/wNumb");
-
-const noUiSlider = require("./vendor/nouislider/nouislider");
-const owlCarousel = require("./vendor/owlcarousel/owl.carousel.js");
