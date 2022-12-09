d834c3d96f546cccb355a38c77aa4da3ff893ba5
jeffrey <jeffreyon11@gmail.com>
2022-12-09 08:58:18 +0100

    trial

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ app.use(
         contentSecurityPolicy: {
             directives: {
                 "img-src": ["*", "'self'", "https://source.unsplash.com"],
-                "script-src": ["'self'", "unsafe-eval"],
+                "script-src": ["'self'", "'unsafe-eval'"],
             },
         },
     })
