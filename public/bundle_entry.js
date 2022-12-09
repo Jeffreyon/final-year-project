9bcf3e750747a78fc94529ff2019635a1d7e5d30
jeffrey <jeffreyon11@gmail.com>
2022-12-09 10:42:54 +0100

    workingg

--- public/bundle_entry.js
+++ public/bundle_entry.js
@@ -1,11 +1,15 @@
 // css files
+require("./vendor/nouislider/nouislider.css");
+require("./vendor/owlcarousel/owl.carousel.css");
+require("./vendor/owlcarousel/owl.theme.green.css");
+
 require("./css/tailwind.css");
 require("./css/custom.css");
 
 // js files
+window.wNumb = require("wnumb");
 window.noUiSlider = require("./vendor/nouislider/nouislider");
 window.owlCarousel = require("./vendor/owlcarousel/owl.carousel");
-window.wNumb = require("./js/wNumb");
 
 require("./js/global");
 require("./js/layouts_auth");
