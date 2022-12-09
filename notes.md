e1681e3083eead8d758513c20115eae36b9dcb52
jeffrey <jeffreyon11@gmail.com>
2022-12-09 09:20:36 +0100

    installed jquery via npm

---
+++ notes.md
@@ -0,0 +1,2 @@
+use helmet to set CSP directives
+add `crossorigin="anonymous"` to img tags to load images from a different origin
--- public/bundle_entry.js
+++ public/bundle_entry.js
@@ -3,7 +3,7 @@ require("./css/tailwind.css");
 require("./css/custom.css");
 
 // js files
-require("./js/jquery");
+require("jquery");
 require("./js/global");
 require("./js/layouts_auth");
 require("./js/layouts_dashboard");
